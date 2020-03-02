ce5ad25fac953bd510b046bb06659ba21b7814e0
Paul McManus <pmcmanus2021@gmail.com>
2020-03-02 10:51:13 +0000

    Capitalizes title and body text

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 function PostItem(props){
+
+  function capitalize(str){
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
   return(
     <div>
-      <h2>{props.title}</h2>
-      <h4>{props.body}</h4>
+      <h2>{capitalize(props.title)}</h2>
+      <h4>{capitalize(props.body)}</h4>
       <button>See comments</button>
     </div>
   )
